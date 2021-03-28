37dd706781062da911438f6a5cb49ddd847524a6
ggnight82 <ggnight82@naver.com>
2021-03-28 19:39:05 +0900

    Detailroom view& serializers

--- anb/rooms/serializers.py
+++ anb/rooms/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 from .models import Room
 from users.serializers import RoomUserSerializers
+
 class RoomSerializer(serializers.ModelSerializer):
 
     user = RoomUserSerializers()
 
     class Meta:
         model = Room
-        fields = ("name", "price", "instant_book", "user")
+        fields = ("pk", "name", "price", "instant_book", "user")
+
+class DetailRoomSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Room
+        fields = "__all__"
--- anb/rooms/views.py
+++ anb/rooms/views.py
@@ -1,8 +1,12 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, DetailRoomSerializer
 
 class ListRoomsView(ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+class SeeRoomView(RetrieveAPIView):
+    queryset = Room.objects.all()
+    serializer_class = DetailRoomSerializer
