34acbdb68ec6cec89c1289290fd5cec2321c9c38
ggnight82 <ggnight82@naver.com>
2021-04-07 19:35:07 +0900

    Search by lat,lng

--- anb/rooms/views.py
+++ anb/rooms/views.py
@@ -80,6 +80,8 @@ def room_search(request):
     beds = request.GET.get("beds", None)
     bedrooms = request.GET.get("bedrooms", None)
     bathrooms = request.GET.get("bathrooms", None)
+    lat = request.GET.get('lat',None)
+    lng = request.GET.get('lng',None)
     filter_kwargs = {}
     if max_price is not None:
         filter_kwargs["price__lte"] = max_price
@@ -91,6 +93,12 @@ def room_search(request):
         filter_kwargs["bedrooms__gte"] = bedrooms
     if bathrooms is not None:
         filter_kwargs["bathrooms__gte"] = bathrooms
+    if lat is not None and lng is not None:
+        filter_kwargs["lat__gte"] = float(lat) - 0.005
+        filter_kwargs["lat__lte"] = float(lat) + 0.005
+        filter_kwargs["lng__get"] = float(lng) - 0.005
+        filter_kwargs["lng__lte"] = float(lng) + 0.005
+
     paginator = PageNumberPagination()
     paginator.page_size = 20
     try: 
